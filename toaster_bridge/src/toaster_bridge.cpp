f6fafd18f35f51c75bd5d7639edd2952f72ba5fb
mfiore <mfiore@adagio.(none)>
2016-01-19 15:15:22 +0100

    first working version

--- toaster_bridge/src/toaster_bridge.cpp
+++ toaster_bridge/src/toaster_bridge.cpp
@@ -1,8 +1,13 @@
 #include <ros/ros.h>
 
+#include <tf/LinearMath/Matrix3x3.h>
+#include <tf/transform_datatypes.h>
+
 #include <toaster_msgs/PutInHand.h>
 #include <toaster_msgs/RemoveFromHand.h>
 #include <toaster_msgs/SetEntityPose.h>
+
+#include <toaster_bridge/agent_position_manager.h>
 
 #include <situation_assessment_msgs/PlaceObject.h>
 #include <situation_assessment_msgs/PutObjectInHand.h>
@@ -14,31 +19,55 @@ ros::ServiceClient toaster_put_in_hand_client_;
 ros::ServiceClient toaster_remove_from_hand_client_;
 ros::ServiceClient toaster_set_entity_pose_client_;
 
+AgentPositionManager* agent_position_manager_;
+
+
+bool simulation_;
+
+
+
 bool putObjectInHand(situation_assessment_msgs::PutObjectInHand::Request &req,
 					 situation_assessment_msgs::PutObjectInHand::Response &res ) 
 {
-	if (req.has_object==true) {
-		toaster_msgs::PutInHand toaster_srv;
-		toaster_srv.req.obect_id=req.object;
-		toaster_srv.req.agent_id=agent;
-		toaster_srv.req.jointName="right_arm";
-		if (toaster_put_in_hand_client_.call(toaster_srv)) {
-			res.result=true;
-		} 
+	if (!simulation_) {
+		if (req.has_object==true) {
+			toaster_msgs::PutInHand toaster_srv;
+			toaster_srv.request.objectId=req.object;
+			toaster_srv.request.agentId=req.agent;
+			toaster_srv.request.jointName="right_arm";
+			if (toaster_put_in_hand_client_.call(toaster_srv)) {
+				res.result=true;
+			} 
+			else {
+				ROS_ERROR("TOASTER_BRIDGE Could not contact toaster");
+				return false;
+			}
+		}
 		else {
-			ROS_ERROR("Could not contact toaster");
-			return false;
+			toaster_msgs::RemoveFromHand toaster_srv;
+			toaster_srv.request.objectId=req.object;
+			if (toaster_put_in_hand_client_.call(toaster_srv)) {
+				res.result=true;
+			} 
+			else {
+				ROS_ERROR("TOASTER_BRIDGE Could not contact toaster");
+				return false;
+			}
 		}
 	}
 	else {
-		toaster_msgs::RemoveFromHand toaster_srv;
-		toaster_srv.req.object_id=req.object;
-		if (toaster_put_in_hand_client_.call(toaster_srv)) {
-			res.result=true;
-		} 
-		else {
-			ROS_ERROR("Could not contact toaster");
-			return false;
+		if (req.has_object==true) {
+			toaster_msgs::SetEntityPose toaster_srv;
+			toaster_srv.request.id=req.object;
+			toaster_srv.request.x=0;
+			toaster_srv.request.y=0;
+			toaster_srv.request.z=0;
+			if (toaster_set_entity_pose_client_.call(toaster_srv)) {
+				return true;
+			}
+			else {
+				ROS_ERROR("TOASTER_BRIDGE Could not contact toaster");
+			}
 		}
 	}
 	return true;
@@ -46,21 +75,74 @@ bool putObjectInHand(situation_assessment_msgs::PutObjectInHand::Request &req,
 bool placeObject(situation_assessment_msgs::PlaceObject::Request &req,
 					 situation_assessment_msgs::PlaceObject::Response &res ) 
 {
+	toaster_msgs::SetEntityPose toaster_srv;
+	toaster_srv.request.id=req.name;
+	toaster_srv.request.x=req.pose.position.x;
+	toaster_srv.request.y=req.pose.position.y;
+	toaster_srv.request.z=req.pose.position.z;
 
-	return true;
+	tf::Quaternion quat;
+	tf::quaternionMsgToTF(req.pose.orientation,quat);
+	tf::Matrix3x3 m(quat);
+	double roll, pitch, yaw;
+	m.getRPY(roll, pitch, yaw);
+
+	toaster_srv.request.roll=roll;
+	toaster_srv.request.pitch=pitch;
+	toaster_srv.request.yaw=yaw;
+
+	if (toaster_set_entity_pose_client_.call(toaster_srv)) {
+		res.result=toaster_srv.response.answer;
+		return true;
+	} 
+	else {
+		ROS_ERROR("TOASTER_BRIDGE Could not contact toaster");
+		return false;
+	}
 }
+
 
 int main(int argc, char** argv) {
 	ros::init(argc,argv,"toaster_bridge");
 
 	ros::NodeHandle node_handle;
+	simulation_=false;
+	node_handle.getParam("/situation_assessment/simulation",simulation_);
 
-	toaster_put_in_hand_client_=node_handle.serviceClient<toaster_msgs::PutInHand>("/pdg/put_in_hand");
-	toaster_set_entity_pose_client_=node_handle.serviceClient<toaster_msgs::SetEntityPose>("/pdg/set_entity_pose");
-	toaster_remove_from_hand_client_=node_handle.serviceClient<toaster_msgs::RemoveFromHand>("/pdg/remove_from_hand");
+	ROS_INFO("TOASTER_BRIDGE Started toaster_bridge");
+	ROS_INFO("TOASTER_BRIDGE Parameters:");
+	ROS_INFO("TOASTER_BRIDGE simulation - %d",simulation_);
+
+
+	agent_position_manager_=new AgentPositionManager(node_handle);
+
+	string module_name;
+	if (simulation_) {
+		module_name="/toaster_simu/";
+	}
+	else {
+		module_name="/pdg/";
+	}
+	toaster_put_in_hand_client_=node_handle.serviceClient<toaster_msgs::PutInHand>(module_name+"put_in_hand");
+	toaster_set_entity_pose_client_=node_handle.serviceClient<toaster_msgs::SetEntityPose>(module_name+"set_entity_pose");
+	toaster_remove_from_hand_client_=node_handle.serviceClient<toaster_msgs::RemoveFromHand>(module_name+"remove_from_hand");
+
+	ROS_INFO("TOASTER_BRIDGE waiting for toaster services");
+	toaster_set_entity_pose_client_.waitForExistence();
+	if (!simulation_) {
+		toaster_put_in_hand_client_.waitForExistence();
+		toaster_remove_from_hand_client_.waitForExistence();
+	}
+	ROS_INFO("TOASTER_BRIDGE connected to toaster services");
+
+
 
 	ros::ServiceServer put_object_in_hand_server=node_handle.advertiseService("situation_assessment/put_object_in_hand",putObjectInHand);
 	ros::ServiceServer place_object_server=node_handle.advertiseService("situation_assessment/place_object",placeObject);
+	ROS_INFO("TOASTER_BRIDGE advertising services");
+	ROS_INFO("TOASTER_BRIDGE Ready");
 
 	ros::spin();
+	delete agent_position_manager_;
+	return 0;
 }
--- toaster_bridge/src/toaster_database_bridge.cpp
+++ toaster_bridge/src/toaster_database_bridge.cpp
@@ -1,8 +1,16 @@
+/*This file acts as a bridge for the toaster database
+Facts are represented in shary as subject predicate[] value[] 
+        		  while in toaster as subject predicate target_id stringValue and other stuff we don't care
+so we need to convert them 
+the problem is that sometimes the target in toaster is represented as a value in shary, and sometimes it's a part of
+the predicate. We take this into account saving this information in a yaml file 
+*/
+
 #include <ros/ros.h>
 
 #include <situation_assessment_msgs/DatabaseRequest.h>
 #include <situation_assessment_msgs/QueryDatabase.h>
-#include <toaster_msgs/GetFacts.h>
+#include <toaster_msgs/ExecuteSQL.h>
 #include <toaster_msgs/RemoveFactToAgent.h>
 #include <toaster_msgs/AddFactToAgent.h>
 
@@ -10,8 +18,15 @@
 #include <vector>
 using namespace std;
 
+//toaster services
 ros::ServiceClient add_fact_toaster, remove_fact_toaster, query_toaster;
 
+map<string,string> target_use_map; //links a predicate (at least the first part of a predicate) to a target use
+string AS_PREDICATE="predicate"; //values of target_use
+string AS_VALUE="value";
+
+string robot_name_;
+//utility functions to split a string
 std::vector<std::string> & stringSplitElems(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
@@ -28,24 +43,31 @@ std::vector<std::string> stringSplit(const std::string &s, char delim) {
 }
 
 
+//Split a predicate vector into a string. Saves also the target if map_target_use[predicate]=predicate
 void getPredicateString(vector<string> predicate, string *property, string *target) {
 	string s="";
 	vector<string> property_predicate;
-	if (predicate.size()>1){
+
+	if (predicate.size()>1 && target_use_map[predicate[0]]==AS_PREDICATE){
 		property_predicate.insert(property_predicate.begin(),predicate.begin(),predicate.end()-1);
-		target=predicate[predicate.size()-1];
+		*target=predicate[predicate.size()-1];
 	}
-	for (int i=0; i<property_predicate.size()-1;i++) {
-		s=s+predicate[i]+"_";
+	else {
+		property_predicate=predicate;
+	}
+	for (int i=0; i<(property_predicate.size()-1);i++) {
+		s=s+property_predicate[i]+"_";
 	}
 	if (property_predicate.size()>0) {
-		s=s+predicate[i];
+		s=s+property_predicate[property_predicate.size()-1];
 	}
-	return s;
+	*property=s;
 }
+//splits a predicate string into a vector. takes into account the use of target
 vector<string> getPredicateVector(string toaster_property, string toaster_target) {
-	vector<string> temp=stringSplit(toaster_property);
-	if (toaster_target!="") {
+	vector<string> temp=stringSplit(toaster_property,'_');
+
+	if (temp.size()>0 && toaster_target!="" && target_use_map[temp[0]]==AS_PREDICATE) {
 		temp.push_back(toaster_target);
 	}
 	return temp;
@@ -54,11 +76,18 @@ vector<string> getPredicateVector(string toaster_property, string toaster_target
 toaster_msgs::Fact convertToToasterFact(situation_assessment_msgs::Fact fact) {
 	toaster_msgs::Fact toaster_fact;
 	toaster_fact.subjectId=fact.subject;
+	ROS_INFO("TOASTER_DATABASE_BRIDGE in convertToToasterFact");
 	getPredicateString(fact.predicate,&toaster_fact.property,&toaster_fact.targetId);
-	if (f.value.size()>0) {
-		toaster_fact.stringValue=f.value[0];
+	ROS_INFO("TOASTER_DATABASE_BRIDGE got predicate string");
+	if (fact.predicate.size()>0 && fact.value.size()>0) {
+		if (target_use_map[fact.predicate[0]]==AS_VALUE){
+			toaster_fact.targetId=fact.value[0];
+		}
+		else  {
+			toaster_fact.stringValue=fact.value[0];
+		}
 	}
-	if (f.value.size()>1) {
+	if (fact.value.size()>1) {
 		ROS_WARN("TOASTER_DATABASE_BRIDGE toaster_database_bridge doesn't support multiple values. Ignoring values after the first");
 	}
 	return toaster_fact;
@@ -67,14 +96,68 @@ toaster_msgs::Fact convertToToasterFact(situation_assessment_msgs::Fact fact) {
 situation_assessment_msgs::Fact convertToSharyFact(toaster_msgs::Fact toaster_fact) {
 	situation_assessment_msgs::Fact result;
 	result.subject=toaster_fact.subjectId;
-	result.predicate=getPredicateVector(toaster_fact.property,toaster_fact.target);
+	result.predicate=getPredicateVector(toaster_fact.property,toaster_fact.targetId);
+	if (target_use_map[result.predicate[0]]==AS_VALUE && toaster_fact.targetId!="") {
+		result.value.push_back(toaster_fact.targetId);
+	}
 	if (toaster_fact.doubleValue!=0) {
 		result.value.push_back(boost::lexical_cast<string>(toaster_fact.doubleValue));
 	}
-	else {
+	else if (toaster_fact.stringValue!=""){
 		result.value.push_back(toaster_fact.stringValue);
 	}
+	
 	return result;
+}
+
+string getQueryString(situation_assessment_msgs::Fact fact) {
+	string subject_sql, predicate_sql, target_sql, stringValue_sql;
+	string predicate_string;
+	string target_string;
+	toaster_msgs::Fact toaster_fact=convertToToasterFact(fact);
+
+	int n=0;
+	if (toaster_fact.subjectId!=""){
+		subject_sql=toaster_fact.subjectId;
+	 	n++;	
+	}
+	if (toaster_fact.property!="") {
+		predicate_sql=toaster_fact.property; 
+		n++;
+	}
+	if (toaster_fact.targetId!="") {
+		target_sql=toaster_fact.targetId; 
+		n++;
+	}
+	if (toaster_fact.stringValue!="") {
+		stringValue_sql=toaster_fact.stringValue; 
+		n++;
+	}
+	string query_string;
+	if (subject_sql!="" || predicate_sql!="" || target_sql!="" || stringValue_sql!="") {
+		query_string=" WHERE ";
+	}
+	if (subject_sql!="") {
+		query_string=query_string+"subject_id='"+subject_sql+"' ";
+		if (n>1) query_string=query_string+" and ";
+		n--;
+	}
+	if (predicate_sql!="") {
+		query_string=query_string+"predicate='"+predicate_sql+"' ";
+		if (n>1) query_string=query_string+" and ";
+		n--;
+	}
+	if (target_sql!="") {
+		query_string=query_string+"target_id='"+target_sql+"' ";
+		if (n>1) query_string=query_string+" and ";
+		n--;
+	}
+	if (stringValue_sql!="") {
+		query_string=query_string+"valueString='"+stringValue_sql+"' ";
+	}
+
+
+	return query_string;
 }
 
 
@@ -83,10 +166,10 @@ bool addFacts(situation_assessment_msgs::DatabaseRequest::Request &req,
 	for (int i=0; i<req.fact_list.size();i++) {
 		situation_assessment_msgs::Fact f=req.fact_list[i];
 		toaster_msgs::AddFactToAgent srv;
-		srv.req.agentId=fact.model;
-		srv.req.fact=convertToToasterFact(f);
+		srv.request.agentId=f.model;
+		srv.request.fact=convertToToasterFact(f);
 		if (add_fact_toaster.call(srv)) {
-			if (!srv.res.answer) {
+			if (!srv.response.answer) {
 				res.report=false;
 				break;
 			}
@@ -104,10 +187,10 @@ bool removeFacts(situation_assessment_msgs::DatabaseRequest::Request &req,
 	for (int i=0; i<req.fact_list.size();i++) {
 		situation_assessment_msgs::Fact f=req.fact_list[i];
 		toaster_msgs::AddFactToAgent srv;
-		srv.req.agentId=fact.model;
-		srv.req.fact=convertToToasterFact(f);
+		srv.request.agentId=f.model;
+		srv.request.fact=convertToToasterFact(f);
 		if (remove_fact_toaster.call(srv)) {
-			if (!srv.res.answer) {
+			if (!srv.response.answer) {
 				res.report=false;
 				break;
 			}
@@ -122,16 +205,34 @@ bool removeFacts(situation_assessment_msgs::DatabaseRequest::Request &req,
 
 bool query(situation_assessment_msgs::QueryDatabase::Request &req,
         situation_assessment_msgs::QueryDatabase::Response &res) {
-	toaster_msgs::GetFacts srv;
-	srv.req.agentId=req.query.model;
-	srv.req.reqFact=convertToToasterFact(req.query);
+	toaster_msgs::ExecuteSQL srv;
+		ROS_INFO("TOASTER_DATABASE_BRIDGE got query");
+
+//	srv.request.agentId=req.query.model;
+//	srv.request.reqFact=convertToToasterFact(req.query);
+	string order="SELECT subject_id, predicate, target_id, valueString from fact_table_"+robot_name_;
+	order=order+getQueryString(req.query);
+	srv.request.order=order;
+	ROS_INFO("Query is %s",order.c_str());
 
 	if (query_toaster.call(srv)) {
-		vector<situation_assessment_msgs::Fact> result;
-		vector<toaster_msgs::Fact> toaster_facts=srv.res.resFactList.fact_list;
-		for (int i=0; i<toaster_facts.size();i++) {
-			situation_assessment_msgs::Fact f=convertToSharyFact(toaster_facts[i]);
-			result.push_back(f);
+		ROS_INFO("response size is %ld",srv.response.results.size());
+		for (int i=0; i<srv.response.results.size();i++) {
+			ROS_INFO("%s",srv.response.results[i].c_str());
+		}
+
+		int i=0;
+		vector<situation_assessment_msgs::Fact> result;		
+		while (i<srv.response.results.size()) {
+			toaster_msgs::Fact toaster_fact;
+			toaster_fact.subjectId=srv.response.results[i];
+			toaster_fact.property=srv.response.results[i+1];
+			toaster_fact.targetId=srv.response.results[i+2];
+			toaster_fact.stringValue=srv.response.results[i+3];
+			situation_assessment_msgs::Fact shary_fact=convertToSharyFact(toaster_fact);
+			shary_fact.model=robot_name_;
+			result.push_back(shary_fact);
+			i=i+4;
 		}
 		res.result=result;
 	}
@@ -146,9 +247,28 @@ int main(int argc,char** argv) {
 	ros::init(argc,argv,"toaster_database_bridge");
 	ros::NodeHandle node_handle;
 
-	add_fact_toaster=node_handle.serviceClient<toaster_msgs/AddFactToAgent>("database/add_faact_to_agent");
-	remove_fact_toaster=node_handle.serviceClient<toaster_msgs/RemoveFactToAgent>("database/add_faact_to_agent");
-	query_toaster=node_handle.serviceClient<toaster_msgs/GetFacts>("database/add_faact_to_agent");
+	vector<string> predicate_list;
+	node_handle.getParam("/situation_assessment/robot_name",robot_name_);
+	ROS_INFO("TOASTER_DATABASE_BRIDGE robot name is %s",robot_name_.c_str());
+	node_handle.getParam("/toaster_bridge/predicate_list",predicate_list);
+	ROS_INFO("TOASTER_DATABASE_BRIDGE got %ld predicates",predicate_list.size());
+	for (int i=0; i<predicate_list.size();i++) {
+		string this_target_use;
+		string param_name="/toaster_bridge/target_use/"+predicate_list[i];
+		node_handle.getParam(param_name,this_target_use);
+		target_use_map[predicate_list[i]]=this_target_use;
+		ROS_INFO("TOASTER_DATABASE_BRIDGE predicate %s is of type %s",predicate_list[i].c_str(),this_target_use.c_str());
+	}
+
+	add_fact_toaster=node_handle.serviceClient<toaster_msgs::AddFactToAgent>("database/add_fact_to_agent");
+	remove_fact_toaster=node_handle.serviceClient<toaster_msgs::RemoveFactToAgent>("database/remove_fact_to_agent");
+	query_toaster=node_handle.serviceClient<toaster_msgs::ExecuteSQL>("database/execute_SQL");
+
+	ROS_INFO("TOASTER_DATABASE_BRIDGE waiting for toaster services");
+	add_fact_toaster.waitForExistence();
+	remove_fact_toaster.waitForExistence();
+	query_toaster.waitForExistence();
+	ROS_INFO("TOASTER_DATABASE_BRIDGE connected to toaster services");
 
 	ros::ServiceServer service_add = node_handle.advertiseService("situation_assessment/add_facts", addFacts);
 	ros::ServiceServer service_remove = node_handle.advertiseService("situation_assessment/remove_facts", removeFacts);
