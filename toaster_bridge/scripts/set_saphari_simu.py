94fba8b5ef2347250c62b82620dabe1c565838ed
mfiore <mfiore@adagio.(none)>
2016-03-03 17:04:16 +0100

    some changes in the simu

--- toaster_bridge/scripts/set_saphari_simu.py
+++ toaster_bridge/scripts/set_saphari_simu.py
@@ -3,6 +3,8 @@
 import sys
 import rospy
 from toaster_msgs.srv import *
+from toaster_msgs.msg import *
+from geometry_msgs.msg import *
 
 def set_pose_client(name,type,x,y,z,roll,pitch,yaw):
   try:
@@ -16,6 +18,35 @@ def set_pose_client(name,type,x,y,z,roll,pitch,yaw):
     print "Service call failed %s"%e
 
 
+def create_area(id,name,center,ray):
+  try:
+    add_area=rospy.ServiceProxy("/area_manager/add_area",AddArea)
+    my_area=toaster_msgs.msg.Area()
+    my_area.id=id
+    my_area.name=name
+    my_area.areaType='room'
+    my_area.entityType='entities'
+    my_area.isCircle=True
+    my_area.center=center
+    my_area.ray=ray
+
+    res=add_area(myArea=my_area)
+  except rospy.ServiceException, e:
+   print "Service call failed %s"%e 
+
+
+   #rosservice call /pdg/manage_stream "{morseHuman: False, niutHuman: False, groupHuman: False, mocapHuman: False, 
+   #adreamMocapHuman: False,
+    # toasterSimuHuman: True, pr2Robot: True, spencerRobot: False, toasterSimuRobot: False,
+     #toasterSimuObject: True}" 
+
+def set_pdg():
+  try:
+    manage_stream=rospy.ServiceProxy("/pdg/manage_stream",AddStream)
+    res=manage_stream(False,False,False,False,False,True,True,False,False,True)
+  except rospy.ServiceException, e:
+    print "Service call failed %s"%e
+
 if __name__=="__main__":
   rospy.wait_for_service("/toaster_simu/set_entity_pose")
   rospy.wait_for_service("/toaster_simu/add_entity")
@@ -27,4 +58,7 @@ if __name__=="__main__":
   set_pose_client("IKEA_SHELF_LIGHT_2","object",-0.3,15.3,0,0,0,-1.5)
   set_pose_client("IKEA_SHELF_LIGHT_3","object",2.3,13.7,0,0,0,4.1) 
   set_pose_client("HERAKLES_HUMAN1","human",-2.2,13.6,0.0,0.0,0.0,0)
-  set_pose_client("PR2_ROBOT","robot",1.3,12.9,0,0,0,2.9)
+#  set_pose_client("PR2_ROBOT","robot",1.3,12.9,0,0,0,2.9)
+  set_pdg()
+  center=Point(-2.2,13.6,0.0)
+  create_area(0,"try",center,5)
